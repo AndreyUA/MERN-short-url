253f402bcb1726c23b3eafcb96193f994be3601e
unknown <infernalsteel666@gmail.com>
2020-10-20 19:28:06 +0300

    auth done

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { useHttp } from "../hooks/http.hook";
+import { useMessage } from "../hooks/message.hook";
+
+const AuthPage = () => {
+  const message = useMessage();
+  const { loading, error, request, clearError } = useHttp();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError]);
+
+  const changeHandler = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request("/api/auth/register", "POST", { ...form });
+      message(data.message);
+    } catch (error) {}
+  };
+
+  const loginHandler = async () => {
+    try {
+      const data = await request("/api/auth/login", "POST", { ...form });
+      message(data.message);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Make your URL shorter.</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authentication</span>
+            <div>
+              <div className="input-field">
+                <input
+                  placeholder="Enter email"
+                  id="email"
+                  type="email"
+                  className="validate"
+                  name="email"
+                  // eslint-disable-next-line
+                  className="yellow-input"
+                  required
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Enter password"
+                  id="password"
+                  type="password"
+                  className="validate"
+                  name="password"
+                  // eslint-disable-next-line
+                  className="yellow-input"
+                  required
+                  onChange={changeHandler}
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button
+              onClick={loginHandler}
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+            >
+              Login
+            </button>
+            <button
+              onClick={registerHandler}
+              className="btn grey lighten-1 black-text"
+              disabled={loading}
+            >
+              Registration
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
+
+
+//1-47-45
+//https://www.youtube.com/watch?v=ivDjWYcKDZI&ab_channel=%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BB%D0%B5%D0%BD%D0%9C%D0%B8%D0%BD%D0%B8%D0%BD
+//https://cloud.mongodb.com/v2/5f8d93f37075e25309e4075e#metrics/replicaSet/5f8d9681ac1125709b4bf534/explorer/app/users/find
