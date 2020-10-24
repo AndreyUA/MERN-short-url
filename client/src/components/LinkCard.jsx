302b4a3bf75206af7a091bd8ad031ba2003e6d75
unknown <infernalsteel666@gmail.com>
2020-10-24 13:31:32 +0300

    10-24-2020

--- client/src/components/LinkCard.jsx
+++ client/src/components/LinkCard.jsx
@@ -31,8 +31,3 @@ const LinkCadr = ({ link }) => {
 };
 
 export default LinkCadr;
-
-
-//2-46
-//https://www.youtube.com/watch?v=ivDjWYcKDZI&ab_channel=%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BB%D0%B5%D0%BD%D0%9C%D0%B8%D0%BD%D0%B8%D0%BD
-//https://cloud.mongodb.com/v2/5f8d93f37075e25309e4075e#metrics/replicaSet/5f8d9681ac1125709b4bf534/explorer/app/users/find
